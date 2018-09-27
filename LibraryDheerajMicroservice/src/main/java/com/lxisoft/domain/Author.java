d1b36b7362a278399a84780ba11da703a078182f
Dheerajdas1992 <dheeraj.das@lxisoft.com>
2018-09-27 11:23:36 +0530

    KAR-105

---
+++ LibraryDheerajMicroservice/src/main/java/com/lxisoft/domain/Author.java
@@ -0,0 +1,125 @@
+package com.lxisoft.domain;
+
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Objects;
+
+/**
+ * A Author.
+ */
+@Entity
+@Table(name = "author")
+public class Author implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "author_name")
+    private String authorName;
+
+    @Column(name = "phone_number")
+    private Long phoneNumber;
+
+    @ManyToMany
+    @JoinTable(name = "author_book",
+               joinColumns = @JoinColumn(name = "authors_id", referencedColumnName = "id"),
+               inverseJoinColumns = @JoinColumn(name = "books_id", referencedColumnName = "id"))
+    private Set<Book> books = new HashSet<>();
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public Author authorName(String authorName) {
+        this.authorName = authorName;
+        return this;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Author phoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public Author books(Set<Book> books) {
+        this.books = books;
+        return this;
+    }
+
+    public Author addBook(Book book) {
+        this.books.add(book);
+        book.getAuthors().add(this);
+        return this;
+    }
+
+    public Author removeBook(Book book) {
+        this.books.remove(book);
+        book.getAuthors().remove(this);
+        return this;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Author author = (Author) o;
+        if (author.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), author.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+            "id=" + getId() +
+            ", authorName='" + getAuthorName() + "'" +
+            ", phoneNumber=" + getPhoneNumber() +
+            "}";
+    }
+}
---
+++ LibraryDheerajMicroservice/src/test/java/com/lxisoft/web/rest/PublisherResourceIntTest.java
@@ -0,0 +1,251 @@
+package com.lxisoft.web.rest;
+
+import com.lxisoft.LibraryApp;
+
+import com.lxisoft.domain.Publisher;
+import com.lxisoft.repository.PublisherRepository;
+import com.lxisoft.web.rest.errors.ExceptionTranslator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+
+import static com.lxisoft.web.rest.TestUtil.createFormattingConversionService;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the PublisherResource REST controller.
+ *
+ * @see PublisherResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LibraryApp.class)
+public class PublisherResourceIntTest {
+
+    private static final String DEFAULT_PUBLISHER_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_PUBLISHER_NAME = "BBBBBBBBBB";
+
+    private static final String DEFAULT_ADDRESS = "AAAAAAAAAA";
+    private static final String UPDATED_ADDRESS = "BBBBBBBBBB";
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    @Autowired
+    private EntityManager em;
+
+    private MockMvc restPublisherMockMvc;
+
+    private Publisher publisher;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        final PublisherResource publisherResource = new PublisherResource(publisherRepository);
+        this.restPublisherMockMvc = MockMvcBuilders.standaloneSetup(publisherResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setConversionService(createFormattingConversionService())
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Publisher createEntity(EntityManager em) {
+        Publisher publisher = new Publisher()
+            .publisherName(DEFAULT_PUBLISHER_NAME)
+            .address(DEFAULT_ADDRESS);
+        return publisher;
+    }
+
+    @Before
+    public void initTest() {
+        publisher = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createPublisher() throws Exception {
+        int databaseSizeBeforeCreate = publisherRepository.findAll().size();
+
+        // Create the Publisher
+        restPublisherMockMvc.perform(post("/api/publishers")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(publisher)))
+            .andExpect(status().isCreated());
+
+        // Validate the Publisher in the database
+        List<Publisher> publisherList = publisherRepository.findAll();
+        assertThat(publisherList).hasSize(databaseSizeBeforeCreate + 1);
+        Publisher testPublisher = publisherList.get(publisherList.size() - 1);
+        assertThat(testPublisher.getPublisherName()).isEqualTo(DEFAULT_PUBLISHER_NAME);
+        assertThat(testPublisher.getAddress()).isEqualTo(DEFAULT_ADDRESS);
+    }
+
+    @Test
+    @Transactional
+    public void createPublisherWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = publisherRepository.findAll().size();
+
+        // Create the Publisher with an existing ID
+        publisher.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restPublisherMockMvc.perform(post("/api/publishers")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(publisher)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Publisher in the database
+        List<Publisher> publisherList = publisherRepository.findAll();
+        assertThat(publisherList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void getAllPublishers() throws Exception {
+        // Initialize the database
+        publisherRepository.saveAndFlush(publisher);
+
+        // Get all the publisherList
+        restPublisherMockMvc.perform(get("/api/publishers?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(publisher.getId().intValue())))
+            .andExpect(jsonPath("$.[*].publisherName").value(hasItem(DEFAULT_PUBLISHER_NAME.toString())))
+            .andExpect(jsonPath("$.[*].address").value(hasItem(DEFAULT_ADDRESS.toString())));
+    }
+    
+    @Test
+    @Transactional
+    public void getPublisher() throws Exception {
+        // Initialize the database
+        publisherRepository.saveAndFlush(publisher);
+
+        // Get the publisher
+        restPublisherMockMvc.perform(get("/api/publishers/{id}", publisher.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(publisher.getId().intValue()))
+            .andExpect(jsonPath("$.publisherName").value(DEFAULT_PUBLISHER_NAME.toString()))
+            .andExpect(jsonPath("$.address").value(DEFAULT_ADDRESS.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingPublisher() throws Exception {
+        // Get the publisher
+        restPublisherMockMvc.perform(get("/api/publishers/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updatePublisher() throws Exception {
+        // Initialize the database
+        publisherRepository.saveAndFlush(publisher);
+
+        int databaseSizeBeforeUpdate = publisherRepository.findAll().size();
+
+        // Update the publisher
+        Publisher updatedPublisher = publisherRepository.findById(publisher.getId()).get();
+        // Disconnect from session so that the updates on updatedPublisher are not directly saved in db
+        em.detach(updatedPublisher);
+        updatedPublisher
+            .publisherName(UPDATED_PUBLISHER_NAME)
+            .address(UPDATED_ADDRESS);
+
+        restPublisherMockMvc.perform(put("/api/publishers")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedPublisher)))
+            .andExpect(status().isOk());
+
+        // Validate the Publisher in the database
+        List<Publisher> publisherList = publisherRepository.findAll();
+        assertThat(publisherList).hasSize(databaseSizeBeforeUpdate);
+        Publisher testPublisher = publisherList.get(publisherList.size() - 1);
+        assertThat(testPublisher.getPublisherName()).isEqualTo(UPDATED_PUBLISHER_NAME);
+        assertThat(testPublisher.getAddress()).isEqualTo(UPDATED_ADDRESS);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingPublisher() throws Exception {
+        int databaseSizeBeforeUpdate = publisherRepository.findAll().size();
+
+        // Create the Publisher
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restPublisherMockMvc.perform(put("/api/publishers")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(publisher)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Publisher in the database
+        List<Publisher> publisherList = publisherRepository.findAll();
+        assertThat(publisherList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    public void deletePublisher() throws Exception {
+        // Initialize the database
+        publisherRepository.saveAndFlush(publisher);
+
+        int databaseSizeBeforeDelete = publisherRepository.findAll().size();
+
+        // Get the publisher
+        restPublisherMockMvc.perform(delete("/api/publishers/{id}", publisher.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate the database is empty
+        List<Publisher> publisherList = publisherRepository.findAll();
+        assertThat(publisherList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    @Transactional
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Publisher.class);
+        Publisher publisher1 = new Publisher();
+        publisher1.setId(1L);
+        Publisher publisher2 = new Publisher();
+        publisher2.setId(publisher1.getId());
+        assertThat(publisher1).isEqualTo(publisher2);
+        publisher2.setId(2L);
+        assertThat(publisher1).isNotEqualTo(publisher2);
+        publisher1.setId(null);
+        assertThat(publisher1).isNotEqualTo(publisher2);
+    }
+}
