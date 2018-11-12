1d209c78d76dc5cfb26b7e97ce268f4900bdd01c
neerajamurali094 <neeraja.m@lxisoft.com>
2018-11-12 10:21:03 +0530

    KAR-286

--- Karma/target/generated-sources/annotations/com/lxisoft/service/mapper/SeverityMapperImpl.java
+++ Karma/target/generated-sources/annotations/com/lxisoft/service/mapper/SeverityMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2018-11-09T15:16:07+0530",
+    date = "2018-11-12T10:02:37+0530",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_131 (Oracle Corporation)"
 )
 @Component
