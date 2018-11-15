deb9b70502d7837b979f569e8d83969eb645f29a
muhammedruhail123 <muhammed.ruhail@lxisoft.com>
2018-11-15 10:44:02 +0530

    KAR-320 Count likes and dislikes on news feed and on comments

--- Karma/src/main/java/com/lxisoft/repository/CommentRepository.java
+++ Karma/src/main/java/com/lxisoft/repository/CommentRepository.java
@@ -1,33 +1,38 @@
 package com.lxisoft.repository;
 
-import com.lxisoft.domain.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.lxisoft.domain.Comment;
 
 /**
- * Spring Data  repository for the Comment entity.
+ * Spring Data repository for the Comment entity.
  */
 @SuppressWarnings("unused")
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-	
-	/* 
+
+	/*
 	 * @param needId
+	 * 
 	 * @return comments with the given needId as page<Comment>
 	 */
 	Page<Comment> findAllByNeedId(Long needId, Pageable pageable);
 
+	// Page<Comment> findByNeedIdIs(Long needId, Pageable pageable);
 
-	//Page<Comment> findByNeedIdIs(Long needId, Pageable pageable);
-
-	
 	Page<Comment> findAllCommentByViolationId(Pageable pageable, Long violationId);
-	
+
 	Page<Comment> findAllCommentByHelpId(Pageable pageable, Long helpId);
 
-	
+	/*
+	 * @param newsFeedId
+	 * 
+	 * @return comments with the given newsFeedId as page<Comment>
+	 */
+
+	Page<Comment> findAllCommentsByNewsFeedId(Pageable pageable, Long newsFeedId);
 
 }
