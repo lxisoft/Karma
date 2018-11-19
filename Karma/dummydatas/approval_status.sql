d6748ed56064ef0e45b7925c3ba39e6be4b67584
sarangibalu <sarangibalu.a@lxisoft.com>
2018-11-19 09:55:45 +0530

    KAR-322

--- Karma/dummydatas/approval_status.sql
+++
@@ -1,4 +0,0 @@
-insert into  approval_status (status) values ('Completed');
-insert into  approval_status (status) values ('Incompleted');
-insert into  approval_status (status) values ('Cancelled');
-
--- Karma/dummydatas/severity.sql
+++
@@ -1,4 +0,0 @@
-insert into severity (severityLevel) values ('low');
-insert into severity (severityLevel) values ('medium');
-insert into severity (severityLevel) values ('urgent');
-insert into severity (severityLevel) values ('very urgent');
