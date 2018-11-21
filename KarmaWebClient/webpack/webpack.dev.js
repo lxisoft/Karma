d7e541cc9491ac4b989e1f089fbe284f2270772c
sanilkumarp <sanilkumar.p@lxisoft.com>
2018-11-22 00:26:17 +0530

    sass removed KAR-378

--- KarmaWebClient/webpack/webpack.dev.js
+++ KarmaWebClient/webpack/webpack.dev.js
@@ -7,7 +7,6 @@ const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
 const path = require('path');
-const sass = require('sass');
 
 const utils = require('./utils.js');
 const commonConfig = require('./webpack.common.js');
@@ -40,7 +39,7 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
     },
     entry: {
         polyfills: './src/main/webapp/app/polyfills',
-        global: './src/main/webapp/content/scss/global.scss',
+        global: './src/main/webapp/content/css/global.css',
         main: './src/main/webapp/app/app.main'
     },
     output: {
@@ -82,21 +81,6 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
                 'angular-router-loader'
             ],
             exclude: ['node_modules']
-        },
-        {
-            test: /\.scss$/,
-            use: ['to-string-loader', 'css-loader', {
-                loader: 'sass-loader',
-                options: { implementation: sass }
-            }],
-            exclude: /(vendor\.scss|global\.scss)/
-        },
-        {
-            test: /(vendor\.scss|global\.scss)/,
-            use: ['style-loader', 'css-loader', 'postcss-loader', {
-                loader: 'sass-loader',
-                options: { implementation: sass }
-            }]
         },
         {
             test: /\.css$/,
