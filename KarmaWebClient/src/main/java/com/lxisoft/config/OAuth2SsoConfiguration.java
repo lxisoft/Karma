474179cb605680503fd133a22c5019bd0e2a72b4
muhammedruhail123 <muhammed.ruhail@lxisoft.com>
2018-12-21 15:12:56 +0530

    security with db

--- KarmaWebClient/src/main/java/com/lxisoft/config/OAuth2SsoConfiguration.java
+++ KarmaWebClient/src/main/java/com/lxisoft/config/OAuth2SsoConfiguration.java
@@ -1,19 +1,30 @@
 package com.lxisoft.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //@EnableOAuth2Sso
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Configuration
 public class OAuth2SsoConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	/* private final CorsFilter corsFilter; */
 
@@ -66,21 +77,30 @@ public class OAuth2SsoConfiguration extends WebSecurityConfigurerAdapter {
 			 */
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&77");
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&77" + new BCryptPasswordEncoder().encode("adminpassword"));
 		httpSecurity.authorizeRequests().antMatchers("/home/pending", "/helps/incompleted").hasAnyRole("ADMIN")
-				.antMatchers("/").hasAnyRole("USER").anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();// .anyRequest()
-		// .permitAll()
-		// .fullyAuthenticated()
-		// .and().formLogin();
+				.antMatchers("/").hasAnyRole("USER").anyRequest().authenticated().and().formLogin().loginPage("/login")
+				.permitAll().and().logout().permitAll()// .anyRequest()
+				// .permitAll()
+				// .fullyAuthenticated()
+				.and().formLogin();
 		// httpSecurity.csrf().disable();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&77");
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth.inMemoryAuthentication().withUser("user").password(encoder.encode("password")).authorities("ROLE_USER")
-				.and().withUser("admin").password(encoder.encode("password")).authorities("ROLE_ADMIN", "ROLE_USER");
+		// PasswordEncoder encoder =
+		// PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(new BCryptPasswordEncoder())
+				.usersByUsernameQuery("select id,password,activated from jhi_user where id=?")
+				.authoritiesByUsernameQuery("select user_id,authority_name from jhi_user_authority where user_id=?");
+		/*
+		 * auth.inMemoryAuthentication().withUser("user").password(encoder.
+		 * encode("password")).authorities("ROLE_USER")
+		 * .and().withUser("admin").password(encoder.encode("password")).
+		 * authorities("ROLE_ADMIN", "ROLE_USER");
+		 */
+
 	}
 }
--- KarmaWebClient/src/main/java/com/lxisoft/service/dto/UserDTO.java
+++ KarmaWebClient/src/main/java/com/lxisoft/service/dto/UserDTO.java
@@ -1,199 +1,200 @@
 package com.lxisoft.service.dto;
 
-import com.lxisoft.config.Constants;
-
-import com.lxisoft.domain.Authority;
-import com.lxisoft.domain.User;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-
-import javax.validation.constraints.*;
 import java.time.Instant;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.lxisoft.config.Constants;
+import com.lxisoft.domain.Authority;
+import com.lxisoft.domain.User;
 
 /**
  * A DTO representing a user, with his authorities.
  */
 public class UserDTO {
 
-    private String id;
+	private String id;
 
-    @NotBlank
-    @Pattern(regexp = Constants.LOGIN_REGEX)
-    @Size(min = 1, max = 50)
-    private String login;
+	@NotBlank
+	@Pattern(regexp = Constants.LOGIN_REGEX)
+	@Size(min = 1, max = 50)
+	private String login;
 
-    @Size(max = 50)
-    private String firstName;
+	@Size(max = 50)
+	private String firstName;
 
-    @Size(max = 50)
-    private String lastName;
+	@Size(max = 50)
+	private String lastName;
 
-    @Email
-    @Size(min = 5, max = 254)
-    private String email;
+	@Size(max = 100)
+	private String password;
 
-    @Size(max = 256)
-    private String imageUrl;
+	@Email
+	@Size(min = 5, max = 254)
+	private String email;
 
-    private boolean activated = false;
+	@Size(max = 256)
+	private String imageUrl;
 
-    @Size(min = 2, max = 6)
-    private String langKey;
+	private boolean activated = false;
 
-    private String createdBy;
+	@Size(min = 2, max = 6)
+	private String langKey;
 
-    private Instant createdDate;
+	private String createdBy;
 
-    private String lastModifiedBy;
+	private Instant createdDate;
 
-    private Instant lastModifiedDate;
+	private String lastModifiedBy;
 
-    private Set<String> authorities;
+	private Instant lastModifiedDate;
 
-    public UserDTO() {
-        // Empty constructor needed for Jackson.
-    }
+	private Set<String> authorities;
 
-    public UserDTO(User user) {
-        this.id = user.getId();
-        this.login = user.getLogin();
-        this.firstName = user.getFirstName();
-        this.lastName = user.getLastName();
-        this.email = user.getEmail();
-        this.activated = user.getActivated();
-        this.imageUrl = user.getImageUrl();
-        this.langKey = user.getLangKey();
-        this.createdBy = user.getCreatedBy();
-        this.createdDate = user.getCreatedDate();
-        this.lastModifiedBy = user.getLastModifiedBy();
-        this.lastModifiedDate = user.getLastModifiedDate();
-        this.authorities = user.getAuthorities().stream()
-            .map(Authority::getName)
-            .collect(Collectors.toSet());
-    }
+	public UserDTO() {
+		// Empty constructor needed for Jackson.
+	}
 
-    public String getId() {
-        return id;
-    }
+	public UserDTO(User user) {
+		this.id = user.getId();
+		this.login = user.getLogin();
+		this.firstName = user.getFirstName();
+		this.lastName = user.getLastName();
+		this.password = user.getPassword();
+		this.email = user.getEmail();
+		this.activated = user.getActivated();
+		this.imageUrl = user.getImageUrl();
+		this.langKey = user.getLangKey();
+		this.createdBy = user.getCreatedBy();
+		this.createdDate = user.getCreatedDate();
+		this.lastModifiedBy = user.getLastModifiedBy();
+		this.lastModifiedDate = user.getLastModifiedDate();
+		this.authorities = user.getAuthorities().stream().map(Authority::getName).collect(Collectors.toSet());
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+	public String getLogin() {
+		return login;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getImageUrl() {
-        return imageUrl;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public boolean isActivated() {
-        return activated;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setActivated(boolean activated) {
-        this.activated = activated;
-    }
+	public String getImageUrl() {
+		return imageUrl;
+	}
 
-    public String getLangKey() {
-        return langKey;
-    }
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
 
-    public void setLangKey(String langKey) {
-        this.langKey = langKey;
-    }
+	public boolean isActivated() {
+		return activated;
+	}
 
-    public String getCreatedBy() {
-        return createdBy;
-    }
+	public void setActivated(boolean activated) {
+		this.activated = activated;
+	}
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
+	public String getLangKey() {
+		return langKey;
+	}
 
-    public Instant getCreatedDate() {
-        return createdDate;
-    }
+	public void setLangKey(String langKey) {
+		this.langKey = langKey;
+	}
 
-    public void setCreatedDate(Instant createdDate) {
-        this.createdDate = createdDate;
-    }
+	public String getCreatedBy() {
+		return createdBy;
+	}
 
-    public String getLastModifiedBy() {
-        return lastModifiedBy;
-    }
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
 
-    public void setLastModifiedBy(String lastModifiedBy) {
-        this.lastModifiedBy = lastModifiedBy;
-    }
+	public Instant getCreatedDate() {
+		return createdDate;
+	}
 
-    public Instant getLastModifiedDate() {
-        return lastModifiedDate;
-    }
+	public void setCreatedDate(Instant createdDate) {
+		this.createdDate = createdDate;
+	}
 
-    public void setLastModifiedDate(Instant lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
 
-    public Set<String> getAuthorities() {
-        return authorities;
-    }
+	public void setLastModifiedBy(String lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
 
-    public void setAuthorities(Set<String> authorities) {
-        this.authorities = authorities;
-    }
+	public Instant getLastModifiedDate() {
+		return lastModifiedDate;
+	}
 
-    @Override
-    public String toString() {
-        return "UserDTO{" +
-            "login='" + login + '\'' +
-            ", firstName='" + firstName + '\'' +
-            ", lastName='" + lastName + '\'' +
-            ", email='" + email + '\'' +
-            ", imageUrl='" + imageUrl + '\'' +
-            ", activated=" + activated +
-            ", langKey='" + langKey + '\'' +
-            ", createdBy=" + createdBy +
-            ", createdDate=" + createdDate +
-            ", lastModifiedBy='" + lastModifiedBy + '\'' +
-            ", lastModifiedDate=" + lastModifiedDate +
-            ", authorities=" + authorities +
-            "}";
-    }
+	public void setLastModifiedDate(Instant lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public Set<String> getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(Set<String> authorities) {
+		this.authorities = authorities;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDTO{" + "login='" + login + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName
+				+ '\'' + ", email='" + email + '\'' + ", imageUrl='" + imageUrl + '\'' + ", activated=" + activated
+				+ ", langKey='" + langKey + '\'' + ", createdBy=" + createdBy + ", createdDate=" + createdDate
+				+ ", lastModifiedBy='" + lastModifiedBy + '\'' + ", lastModifiedDate=" + lastModifiedDate
+				+ ", authorities=" + authorities + "}";
+	}
 }
